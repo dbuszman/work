d1658d21df16761d48a812889edd877519c4b1e8
Darek <dariusz.b123@gmail.com>
2015-03-26 09:35:09 +0100

    Create TimeChange.bat
    Zmiana czasu systemowego

---
+++ TimeChange.bat
@@ -0,0 +1,11 @@
+ rem Name: TimeChange.bat
+ rem Author: DB
+ rem Zmienia czas systemowy na 10 pazdziernik 2002
+ rem Nalezy utworzyc skrot i w Wlasciwosciach: Skrot -> Zaawansowane -> Uruchom jako administrator
+ 
+ 
+@echo off
+
+date 02-10-10
+
+GOTO:EOF
