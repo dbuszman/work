9f12aebc502708e92bb897287641be72256e5d8b
Dariusz Buszman <dariusz.b123@gmail.com>
2015-04-23 10:56:27 +0200

    Create MaximalizeConsole.bat

---
+++ MaximalizeConsole.bat
@@ -0,0 +1,7 @@
+ rem Name: MaximalizeConsole.bat
+ rem Author: DB
+ rem Uruchamianie ConsoleApplication1.exe w FullScreenMode
+ rem Nalezy utworzyc skrot i w Wlasciwosciach: Skrot -> Zaawansowane -> Uruchom jako administrator
+ 
+@echo off
+start /MAX C:\Users\Dariusz\Desktop\ConsoleApplication1.exe 
